ac9da3449cc9a2f1c75cf1e86816ada69361fc0e
Abhishek Udupa <56753465+abhishekaudupa@users.noreply.github.com>
2024-03-11 19:31:52 +0530

    Add sample counter program

---
+++ sample-programs/counter.asm
@@ -0,0 +1,6 @@
+LDI 1     	//load the value 1 into A-Register.
+STA 15    	//Store A-Register's contents in memory location 15.
+LDI 0		//load the value 0 into A-Register.
+ADD 15    	//Add the contents of mem. loc. 15 into A-Register.
+OUT       	//Display contents of A-Register on SSD.
+JMP 3     	//Jump to mem. loc. 2.
